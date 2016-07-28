b6d197225a4ee6fddcf6bfe073f4d42c506603bf
fionser <fionser@gmail.com>
2016-07-28 17:32:21 +0900

    randomly generate data

--- core/test/TestContingencyTable.cpp
+++ core/test/TestContingencyTable.cpp
@@ -6,11 +6,51 @@
 #include "HElib/Ctxt.h"
 #include "HElib/FHE.h"
 #include "HElib/EncryptedArray.h"
+#include <list>
+#include <map>
 #ifdef FHE_THREADS
 #define NR_THREADS 8
 #else
 #define NR_THREADS 1
 #endif
+struct Pair_t {
+	long u;
+	long v;
+	Pair_t(long u = 0, long v = 0) : u(u), v(v) {}
+	bool operator==(const Pair_t &b) const {
+		return u == b.u && v == b.v;
+	}
+	bool operator<(const Pair_t &b) const {
+		return u < b.u && v < b.v;
+	}
+};
+
+
+std::list<std::vector<long>> 
+generate_data(core::Attribute P, core::Attribute Q, long N, long slots) {
+	std::list<std::vector<long>> data;
+	std::map<Pair_t, int> counting;
+	for (long i = 0; i < N; i++) {
+		std::vector<long> row(slots, 0);
+	   	long u = static_cast<long>(NTL::RandomBnd(P.size));
+	   	long v = static_cast<long>(NTL::RandomBnd(Q.size));
+		Pair_t pp(u, v);
+		const auto& kv = counting.find(pp);
+		if (kv == counting.end())
+			counting.insert({pp, 1});
+		else
+			kv->second += 1;
+		row.at(P.offset + u) = 1;
+		row.at(Q.offset + v) = 1;
+		data.push_back(row);
+	}
+	printf("generated data\n");
+	for (auto &pp : counting) {
+		printf("%ld %ld %d\n", pp.first.u, pp.first.v, pp.second);
+	}
+	return data;
+}
+
 void test_CT(const long N) {
     long parameters[][2] = {
             {8219, 77933}, // 80-bits
@@ -31,26 +71,19 @@ void test_CT(const long N) {
     addSome1DMatrices(*sk);
     core::pk_ptr pk = std::make_shared<FHEPubKey>(*sk);
 
-    auto ea = context->ea;
-    std::vector<long> slots(ea->size(), 0);
-    // Attribute P with size = 2
-    slots[0] = 1; slots[1] = 1;
-    // Attribute Q with size = 4
-    slots[2] = 0; slots[3] = 0; slots[4] = 1; slots[5] = 0;
-    // Attribute R with size = 4
-    slots[6] = 0; slots[7] = 1; slots[8] = 0; slots[9] = 0;
-
-    Ctxt _tmp(*pk);
-    ea->encrypt(_tmp, *pk, slots);
-    std::vector<Ctxt> ctxts(N, _tmp);
-
     auto type = core::Attribute::Type::CATEGORICAL;
     struct core::Attribute P = { .text = "P", .type = type, .size = 2, .offset = 0};
-    struct core::Attribute Q = { .text = "Q", .type = type, .size = 3, .offset = 2};
+    struct core::Attribute Q = { .text = "Q", .type = type, .size = 4, .offset = 2};
     struct core::Attribute R = { .text = "R", .type = type, .size = 4, .offset = 6};
-    // struct core::Attribute P = { text : "P", type: type, size : 2, offset : 0};
-    // struct core::Attribute Q = { text : "Q", type: type, size : 4, offset : 2};
-    // struct core::Attribute R = { text : "R", type: type, size : 4, offset : 6};
+
+    auto ea = context->ea;
+    auto _data = generate_data(P, Q, N, ea->size());
+    std::vector<Ctxt> ctxts(_data.size(), *pk);
+    size_t idx = 0;
+    for (auto &row : _data) {
+        ea->encrypt(ctxts.at(idx), *pk, row);
+	idx += 1;
+    }
 
     auto helper = new core::PrivateContingencyTableHelper(P, Q, 5, ea);
     core::PrivateContingencyTable CT(context, helper);
@@ -65,7 +98,7 @@ void test_CT(const long N) {
     auto counts = helper->final_decrypt(n_uv, publishable, sk, ea);
     FHE_NTIMER_STOP(Decryption);
 
-    printf("Evaluated %ld records\n", N);
+    printf("Evaluated %ld records\n", ctxts.size());
     auto modified = core::coprime(P.size, Q.size);
     for (size_t x = 0; x < counts.size(); x++) {
         printf("%lu %lu -> %ld\n", x % modified.first, x % modified.second, counts.at(x));
